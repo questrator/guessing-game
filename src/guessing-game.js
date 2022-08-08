713faf35e81f94d4d69684df83c23aefabe33842
Slava Ivanov <drumtrack@ya.ru>
2022-08-08 14:15:29 +0300

    done: complete task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,26 @@
 class GuessingGame {
     constructor() {}
 
-    setRange(min, max) {
+    result = 0;
+    min = -Infinity;
+    max = +Infinity;
 
+    setRange(min, max) {
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.result = Math.round((this.max + this.min) / 2);
+        return this.result;
     }
 
     lower() {
-
+        this.setRange(this.min, this.result);
     }
 
     greater() {
-
+        this.setRange(this.result, this.max);
     }
 }
 
